0c43781622a216401c51d9adb07dacc7eb561148
Piyush <piyush2521@gmail.com>
2023-01-15 23:07:03 -0600

    new files

---
+++ minimize_sum_product.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def minValue(self, a, b, n):
+        # Your code goes here
+        a.sort()
+        b.sort(reverse = True)
+        sum_product = 0
+        for i in range(len(a)):
+            sum_product += a[i]*b[i]
+        
+        return sum_product
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob=Solution()
+        print(ob.minValue(a, b, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
